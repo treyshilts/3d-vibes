b4e0faca89314aabc5f6b273ca87d6d6900fdf32
trey <67873802+treyshilts@users.noreply.github.com>
2025-01-17 16:48:56 -0800

    Update game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('threejs-scene');
     const scene = new THREE.Scene();
 
-    scene.fog = new THREE.Fog(0xaaaaaa, 3, 40);
+    scene.fog = new THREE.Fog(0xaaaaaa, 1, 400);
 
     // Add the sky sphere
     const skyTexture = new THREE.TextureLoader().load('https://treyshilts.github.io/3d-vibes/orangesky2.png');
