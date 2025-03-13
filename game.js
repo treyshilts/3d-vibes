e5a18dfd73755043ebdcd7a81a9a1d7f46e7d378
trey <67873802+treyshilts@users.noreply.github.com>
2025-03-12 22:05:33 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1417,7 +1417,7 @@ const loadMap = () => {
 
 
     mapLoader.load(
-        'https://treyshilts.github.io/3d-vibes/finalmap_summer3d_2.glb',
+        'https://treyshilts.github.io/3d-vibes/finalmap_summer3d_3.glb',
         (gltf) => {
             const map = gltf.scene;
 
